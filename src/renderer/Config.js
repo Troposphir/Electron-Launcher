a79dfc268a9e5d5cc157b25300bcdf8ad5533677
Adam Gaskins <adamg@writemoreorders.com>
2019-09-01 20:09:28 -0400

    Mac path compatibility

--- src/renderer/Config.js
+++ src/renderer/Config.js
@@ -1,8 +1,17 @@
 import fs from 'fs'
+import path from 'path'
+import os from 'os'
+import { remote } from 'electron'
 
 export default class Config {
     constructor() {
         this.fileName = 'config.json'
+        if (os.platform() === 'darwin') {
+            this.fileName = path.join(
+                remote.app.getPath('userData'),
+                'config.json'
+            )
+        }
         this.data = {}
         if (fs.existsSync(this.fileName))
             this.data = JSON.parse(fs.readFileSync(this.fileName))
@@ -10,7 +19,17 @@ export default class Config {
         if (!this.has('version')) this.set('version', '0.0.0')
         if (!this.has('launch'))
             this.set('launch', ['Atmosphir_Data/Atmosphir.exe', 'standalone'])
-        if (!this.has('asset_path')) this.set('asset_path', 'game/')
+        if (!this.has('asset_path')) {
+            if (os.platform() === 'darwin') {
+                this.set(
+                    'asset_path',
+                    path.join(remote.app.getPath('userData', 'game') + '/')
+                )
+            } else {
+                this.set('asset_path', 'game/')
+            }
+        }
+        console.log(remote.app.getPath('userData'))
         if (!this.has('server')) {
             if (process.env.NODE_ENV === 'production')
                 this.set(
